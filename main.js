ff9c3b0ce0215c3431d5e3c204311266161b09b7
yellowcurryninja <fardin5215@gmail.com>
2024-09-28 01:33:21 +0600

    give rules of the game
    gave if else statements and a score display in the code for the game tor run

--- main.js
+++ main.js
@@ -18,3 +18,48 @@ if (getHumanChoice === "rock" || getHumanChoice === "paper" || getHumanChoice ==
 // making the scoreboard
 humanScore = 0
 computerScore = 0
+
+// making the game rules
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert("Its a tie")
+        alert('You have a score of ' + humanScore + 'and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "rock" && computerChoice == "paper") {
+        alert("You lose! Computer choose paper and paper beats rock")
+        computerScore++
+        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "rock" && computerChoice == "scissor") {
+        alert("You win! Computer choose scissor and rock beats scissor")
+        humanScore++
+        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "paper" && computerChoice == "rock") {
+        alert("You win! Computer choose rock and paper beats rock")
+        humanScore++
+        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "paper" && computerChoice == "scissor") {
+        alert("You lose! Computer choose scissor and scissor beats paper")
+        computerScore++
+        alert('You have a score of '+humanScore +' and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "scissor" && computerChoice == "rock") {
+        alert("You lose! Computer choose rock and rock beats scissor")
+        computerScore++
+        alert('You have a score of '+humanScore +' and computer has a score of '+computerScore)
+    }
+    else if (humanChoice === "scissor" && computerChoice == "paper") {
+        alert("You win! Computer choose paper and scissor beats paper")
+        humanScore++
+        alert('You have a score of '+humanScore+ ' and computer has a score of ' + computerScore)
+    }
+    
+} 
+const humanSelection = getHumanChoice;
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
+
+
