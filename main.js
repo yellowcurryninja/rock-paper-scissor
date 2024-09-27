8cecaf9e636a5575d7e39ac0bb610a13ad6500fa
yellowcurryninja <fardin5215@gmail.com>
2024-09-27 22:41:19 +0600

    make Human Choice and Computer Choice function
    added a list(game) where the item resides, getComputerChoice function randomly takes one from it, getHumanChoice function was made by using promt where puts rock paper or scissor

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+// make a list of them and add it to game variable
+game = ["rock","paper","scissor"]
+
+// let the  computer choose between a random item from the game list
+function getComputerChoice() {
+    return game.at(Math.floor(Math.random() * 3))
+}
+
+// let the user choose between rock, paper or scisso
+let getHumanChoice = prompt("choose ur pick rock, paper or scissor").toLowerCase()
+
+if (getHumanChoice === "rock" || getHumanChoice === "paper" || getHumanChoice === "scissor") {
+    alert("you choose " + getHumanChoice)
+}   else {
+    alert("Invalid input, please only choose between rock, paper or scissor")
+}
+
+// making the scoreboard
+humanScore = 0
+computerScore = 0
