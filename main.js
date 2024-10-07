302dc6b25761bf59b5d01b2a1c19e0d26aa4e000
yellowcurryninja <fardin5215@gmail.com>
2024-10-07 10:12:54 +0600

    remove loop 5 times, add buttons, eventlistener on buttons

--- main.js
+++ main.js
@@ -23,38 +23,38 @@ function playRound (humanChoice, computerChoice) {
 
 
     if (humanChoice === computerChoice) {
-        alert("Its a tie")
-        alert('You have a score of ' + humanScore +' and computer has a score of '+computerScore)
+        console.log("Its a tie")
+        console.log('You have a score of ' + humanScore +' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "rock" && computerChoice == "paper") {
-        alert("You lose! Computer choose paper and paper beats rock")
+        console.log("You lose! Computer choose paper and paper beats rock")
         computerScore++
-        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+        console.log('You have a score of '+humanScore+' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "rock" && computerChoice == "scissor") {
-        alert("You win! Computer choose scissor and rock beats scissor")
+        console.log("You win! Computer choose scissor and rock beats scissor")
         humanScore++
-        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+        console.log('You have a score of '+humanScore+' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "paper" && computerChoice == "rock") {
-        alert("You win! Computer choose rock and paper beats rock")
+        console.log("You win! Computer choose rock and paper beats rock")
         humanScore++
-        alert('You have a score of '+humanScore+' and computer has a score of '+computerScore)
+        console.log('You have a score of '+humanScore+' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "paper" && computerChoice == "scissor") {
-        alert("You lose! Computer choose scissor and scissor beats paper")
+        console.log("You lose! Computer choose scissor and scissor beats paper")
         computerScore++
-        alert('You have a score of '+humanScore +' and computer has a score of '+computerScore)
+        console.log('You have a score of '+humanScore +' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "scissor" && computerChoice == "rock") {
-        alert("You lose! Computer choose rock and rock beats scissor")
+        console.log("You lose! Computer choose rock and rock beats scissor")
         computerScore++
-        alert('You have a score of '+humanScore +' and computer has a score of '+computerScore)
+        console.log('You have a score of '+humanScore +' and computer has a score of '+computerScore)
     }
     else if (humanChoice === "scissor" && computerChoice == "paper") {
-        alert("You win! Computer choose paper and scissor beats paper")
+        console.log("You win! Computer choose paper and scissor beats paper")
         humanScore++
-        alert('You have a score of '+humanScore+ ' and computer has a score of ' + computerScore)
+        console.log('You have a score of '+humanScore+ ' and computer has a score of ' + computerScore)
     }
     
 }
@@ -66,13 +66,11 @@ function playGame () {
     // making the scoreboard
     humanScore = 0
     computerScore = 0
+        
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
     
-    // looping the fucntion for 5 times
-    for (let i=0; i <= 4; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection,computerSelection)
-    }
     if (humanScore > computerScore) {
         alert("Congratulations you won!")
     } else {
@@ -80,4 +78,7 @@ function playGame () {
     }
 }
 
-playGame()
+// playGame()
+
+const buttons = document.querySelectorAll("button")
+buttons.addEventListener("click", playGame)
